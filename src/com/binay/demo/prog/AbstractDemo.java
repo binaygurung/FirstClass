2ca31234b2dcbc6fd7950f5a8330b2b58237680b
binaygurung <binay.gurung@gmail.com>
2022-03-16 23:39:20 -0500

    Inheritance Demo Examples

---
+++ src/com/binay/demo/prog/AbstractDemo.java
@@ -0,0 +1,51 @@
+package com.binay.demo.prog;
+
+public class AbstractDemo {
+    public static void main(String[] args) {
+//        Bird animal = new Dog();
+//        animal.sound();
+//
+//        animal = new Cat();
+//        animal.sound();
+
+        printSound(new Dog());
+        printSound(new Cat());
+    }
+
+    static void printSound(AnimalNew animal) {
+        animal.sound();
+        animal.isAlive();
+    }
+}
+
+
+abstract class AnimalNew {
+    public abstract void sound();
+
+    public void isAlive(){
+        System.out.println("Alive");
+    }
+}
+
+
+abstract class Bird extends AnimalNew {
+}
+
+class Dog extends Bird {
+
+    @Override
+    public void sound() {
+        System.out.println("Bark");
+    }
+
+}
+
+
+class Cat extends Bird {
+
+    @Override
+    public void sound() {
+        System.out.println("Meow");
+    }
+
+}
